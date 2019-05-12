3746a15f17cbd3b59e26ef4c7c178fbb988ea4ce
zubtsov.r <zubtsov.r@gmail.com>
2019-05-12 21:31:47 +0300

    temporary commit

--- src/main/java/zubtsov/VelocityMerger.java
+++ src/main/java/zubtsov/VelocityMerger.java
@@ -2,22 +2,21 @@ package zubtsov;
 
 import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.Velocity;
 import org.apache.velocity.app.VelocityEngine;
-import org.apache.velocity.runtime.RuntimeConstants;
 
-import java.io.File;
 import java.io.Writer;
 import java.util.Map;
+import java.util.Properties;
 
 public class VelocityMerger {
     public void merge(String templateFilePath, Map<String, Object> contextObjects, Writer writer) {
         VelocityEngine ve = new VelocityEngine();
-        ve.init();
-        ve.setProperty(RuntimeConstants.RESOURCE_LOADERS, "file,classpath");
-        ve.setProperty(RuntimeConstants.RESOURCE_LOADER_PATHS,
-                new File(templateFilePath).getParent());
+        Properties p = new Properties();
+        p.put(Velocity.FILE_RESOURCE_LOADER_PATH, "");
+        ve.init(p);
 
-        Template t = ve.getTemplate(new File(templateFilePath).getName(), "utf-8");
+        Template t = ve.getTemplate(templateFilePath, "UTF-8");
 
         VelocityContext context = new VelocityContext();
         contextObjects.forEach((str, obj) -> context.put(str, obj));
